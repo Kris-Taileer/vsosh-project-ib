0162e9267145e233d8ae794ef6ec5ea4f6218440
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2026-01-06 22:30:55 +0300

    Update db.py

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ class DataBase:
 
     def add_user(self, user_id):
         with self.connection:
-            self.cursor.execute("INSERT INTO 'users' ('user_id') VALUE (?)", (user_id,))
+            return self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
 
     def user_exists(self, user_id):
         with self.connection:
@@ -21,3 +21,10 @@ class DataBase:
     def get_signup(self, user_id):
         with self.connection:
             result = self.cursor.execute("SELECT 'signup' FROM 'users' WHERE 'user_id' = ?", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+    def set_username(self, user_id, signup):
+        with self.connection:
+            return self.cursor.execute("UPDATE 'users' SET 'signup' = ? WHERE 'user_id' = ?", (signup, user_id,))
