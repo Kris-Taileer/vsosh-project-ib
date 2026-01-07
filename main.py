d680a7d1798fb176fc08c09d153f337823367023
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2026-01-07 10:08:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,11 @@ token = os.getenv('TOKEN')
 bot = telebot.TeleBot(token=token)
 conn = sqlite3.connect('users.db', check_same_thread=False)
 cursor = conn.cursor()
-
-
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
-    singups VARCHAR DEFAULT setusername
+    singup VARCHAR DEFAULT 'setusername'
 )
 ''')
 conn.commit()
@@ -27,26 +25,22 @@ db = DataBase('users.db')
 
 
 @bot.message_handler(content_types=['text'])
-def start(message):
-    if (not db.user_exists(message.from_user.id)):
-        db.add_user(message.from_user.id)
-        bot.send_message(message.from_user.id, "Укажите Юзернейм:")
-    else:
-        bot.send_message(message.from_user.id, "Вы уже зарегестрированы") #reply markup needed
+def handle_message(message):
+    user_id = message.from_user.id
 
-
-@bot.message_handler(content_types=['text'])
-def bot_message(message):
-    if db.get_signup(message.from_user.id) == "setusername":
-        if (len(message.text) > 15):
-            bot.send_message(message.from_user.id, "Никнейм не должен привышать 15 символов")
+    if not db.user_exists(user_id):
+        db.add_user(user_id)
+        bot.send_message(user_id, "Укажите Юзернейм:")
+    elif db.get_signup(user_id) == "setusername":
+        if len(message.text) > 15:
+            bot.send_message(user_id, "Никнейм не должен превышать 15 символов")
         elif '@' in message.text or '/' in message.text:
-            bot.send_message(message.from_user.id, "Вы ввели запрещённый символ")
+            bot.send_message(user_id, "Вы ввели запрещённый символ")
         else:
-            db.set_username(message.from_user.id, message.text)
-            db.set_signup(message.from_user.id, "done")
-            bot.send_message(message.from_user.id, "Регистрация завершена успешно")
+            db.set_username(user_id, message.text)
+            db.set_signup(user_id, "done")
+            bot.send_message(user_id, "Регистрация завершена успешно")
     else:
-        bot.send_message(message.from_user.id, "Что?")
+        bot.send_message(user_id, "Вы уже зарегистрированы.")
 
 bot.polling(none_stop=True, interval=0)
